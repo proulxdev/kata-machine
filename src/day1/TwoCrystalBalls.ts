dbd31c45bc7a0b0a95a3c33e0b1e8f4781c1934a
Cameron Proulx <cameronproul@umass.edu>
2025-08-22 14:27:30 -0400

    Updated TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -6,7 +6,7 @@ export default function two_crystal_balls(breaks: boolean[]): number {
             break;
         }
     }
-    for (let j = i - jumpAmount; j < breaks.length; j++) {
+    for (let j = i - jumpAmount; j < i; j++) {
         if (breaks[j] == true) {
             return j;
         }
