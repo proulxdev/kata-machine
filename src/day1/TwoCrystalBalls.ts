8f189466340d47faa4c2384ea14c02a0027bb61e
Cameron Proulx <cameronproul@umass.edu>
2025-08-22 14:13:55 -0400

    Solved TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i] == true) {
+            break;
+        }
+    }
+    for (let j = i - jumpAmount; j < breaks.length; j++) {
+        if (breaks[j] == true) {
+            return j;
+        }
+    }
+    return -1;
+    // O(sqrt(n))
 }
